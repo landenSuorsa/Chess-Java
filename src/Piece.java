1d85175ee7d947c97853c1e4ab52174624e4204a
JackH006 <152431618+JackH006@users.noreply.github.com>
2023-12-09 12:26:52 -0500

    Update Piece.java

--- src/Piece.java
+++ src/Piece.java
@@ -3,19 +3,12 @@ import java.util.ArrayList;
 public class Piece {
     public String type;
     public int player;
-    public int row, col;
-    public int xPos, yPos;
-    public int value;
     
 
-    public Piece(String type, int player, int row, int col, int xPos, int yPos, int value) {
+    public Piece(String type, int player) {
         this.type = type;
         this.player = player;
-        this.row = row;
-        this.col = col;
-        this.xPos = xPos;
-        this.yPos = yPos;
-        this.value = value;
+       
     }
 
     public String getType() {return type; }
